26be918098cd0842d0c7875c3c76edd45d1d5360
taahmed <46457860+taahmed@users.noreply.github.com>
2019-10-26 19:04:46 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<title>Taher Ahmed : WEB120 Portal Website</title>
+<meta name="viewport" content="width=device-width" />
+<meta name="robots" content="noindex,nofollow" />
+<meta charset="utf-8">
+<script src="https://use.fontawesome.com/6a71565c22.js"></script>
+<link rel="stylesheet" href="css/nav.css" />
+<link rel="stylesheet" href="css/portal.css" />
+<link rel="stylesheet" href="css/form.css" />
+<link rel="stylesheet" href="css/tables.css" />
+</head>
+<body>
+<div class="wrapper">
+<header>
+  <h1><a href="index.php"><i class="logo fa fa-home"></i> Taher Ahmed SCC WEB120 Portal</a></h1>
+ <nav>
+    <ul class="topnav" id="myTopnav">
+       <li><a href="index.php" class="selected">Welcome</a></li>
+       <li><a href="table.php">Table</a></li>
+	   <li><a href="big/index.php">Big</a></li>
+	   <li><a href="aia.php">AIA</a></li>
+	   <li><a href="wireframe.php">Wireframe</a></li>
+	   <li><a href="fp/index.php">Final Project</a></li>
+	   <li><a href="index.php">Contact Tahero</a></li>      
+
+	<li class="icon"><a href="javascript:void(0);" onclick="myFunction()">&#9776;</a></li>
+    </ul>
+  </nav>
+ 
+</header>
+    <!-- START LEFT COL -->
+<section>
+ <h2 class="pageID">Welcome</h2>
+<!--END HEADER INCLUDE FILE        -->
+
+ <img src="images/desktop.jpg" class="desktop" alt="tahero family photos" />
+ <img src="images/phone.jpg" class="phone" alt="Nizu's photo" />
+ 
+ <h3>Welcome to my page @ Seattle Central College</h3>
+    <p>Hello! My name is Taher Ahmed and I moved to Seattle 2 years ago with my wife and my lovely daughter from the <b>13 months</b>
+ sunshine country Ethiopia, Africa.</p>
+        
+    <p>I was born in Mekelle town, Tigray region, Ethiopia. I grew up in Mekelle. In Seattle, after six months working with out profession,
+ I have decided to continue education. In order to work professionally, and become a role model for my daughter I decided to return back to education.
+ I devoted my self to grueling practices daily throughout the year and developed an effective time-managemet, through which I balanced school, 
+family and ful-time job while dedicating myself fully to both. As parents are a big role models for the good and for the bad deeds.<p>
+		
+<h3> My Hobbies</h3>
+
+<p> My plan is to complete the Web development and Database Administration in double major in Seattle Central College. 
+I hope to become all rounded Full stack software Developer or Front end and back end developer. 
+It includes developing Websites, developing and administering databases, softare applications, mobile spplications
+ and connecting with database. I hope to continue education, even  with some challenges and in my old age also.
+ It certainly have become more than a hobby and more of a daily pracetice of passion and wellness.  </p>
+        <b><q>See you at the top and stay there.</q></b> <br><br>
+     </section>   
+	
+<!-- END LEFT COL (main) -->
+<!-- START RIGHT COL (aside) -->
+<aside>
+ <h3> About My Daughter (Nizam) </h3>
+ <img src="images/tablet.jpg" class="tablet" alt=" Afar father's culture, Ethiopia" />
+ <p>Nizam is my lovely daughter. she is <b>six</b> years old. She had great friends and became an awesome Student in Seattle! 
+She loved running on Tracks,Parks and playing soccer and playing mobile games. she had no fear! Her favorite thing about 
+the city and our community is, there are many parks around our apartment!</p>
+
+ <p>Playing with her is lovely, an amazing dancer and runner as well as friendly face on Yesler Seattle, Washington. 
+When Nizam miss her country and her grandma and grndfa and our family, she cries and I wrote poems about my family, my country and about her. 
+It greatly touched my heart that my daughter needs to communicate always and she try to call using video calls to back home to express herself
+ and her feelings and she try to represent in her art about her background.</p>
+    
+    
+</aside>
+
+    
+<!-- END RIGHT COL (aside) -->
+<!--start bottom include file-->
+<!-- START Footer -->
+<footer>
+  <p><small>Designed by <a href="#" target="_blank">Taher Ahmed &copy; 2019 </a>, All Rights Reserved ~ <a href="https://validator.w3.org/nu/?doc=http%3A%2F%2Ftaheraa.com%2Fweb%2Fweb120%2Findex.php">Valid HTML!</a> ~ <a href="https://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Ftaheraa.com%2Fweb%2Fweb120%2Fcss%2Fportal.css&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en" target="_blank">Valid CSS</a></small></p>
+</footer>
+<!-- END Footer --> 
+<script>
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+</script>
+</div>
+</body>
+</html>
